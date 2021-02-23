4e5f8510c6f965fc2fac03d0822c7d6d6d9e2035
Ben Lakey <blakey@axon.com>
2021-02-22 18:03:30 -0800

    Improve dotfiles. Don't store vscode extensions. Temporarily remove linux capabilities for vscode. Provide updated instructions on usage.

--- symlinks/.vscode/extensions/octref.vetur-0.22.1/server/dist/modes/template/services/vueInterpolationCompletion.d.ts
+++
@@ -1,3 +0,0 @@
-import { CompletionList } from 'vscode-languageserver';
-import { VueFileInfo } from '../../../services/vueInfoService';
-export declare function doVueInterpolationComplete(vueFileInfo: VueFileInfo): CompletionList;
